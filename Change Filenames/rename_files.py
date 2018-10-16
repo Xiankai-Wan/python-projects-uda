83fa78ed4ef8cc39bf3dc19fd84a43cbe0ce0db6
Kevin-Wan <xiankai.wan@gmail.com>
2018-10-16 09:20:18 +0800

    completed

--- Change Filenames/rename_files.py
+++ Change Filenames/rename_files.py
@@ -1,9 +1,14 @@
+import os
 def rename_files():
     """
     1. get file names from a folder
     2. for each file, rename filename
     """
-
-
-
+    file_list = os.listdir('/Users/kevin_wan/Documents/python-projects-uda/Change Filenames')
+    print(file_list)
+    saved_path = os.getcwd();
+    os.chdir('/Users/kevin_wan/Documents/python-projects-uda/Change Filenames/prank')
+    for file_name in file_list:
+        os.rename(file_name,file_name.translate(None,'0123456789'))
+    os.chdir('/Users/kevin_wan/Documents/python-projects-uda/Change Filenames')
 rename_files()
